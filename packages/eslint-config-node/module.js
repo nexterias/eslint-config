139ccdae209a29051b464ce825a701cbb5cff737
InkoHX <me@inkohx.dev>
2022-11-26 00:30:08 +0900

    fix: remove `module` and `script` rules

--- packages/eslint-config-node/module.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-/** @type {import('eslint').Linter.Config} */
-const config = {
-  plugins: ['n'],
-  extends: [require.resolve('.'), 'plugin:n/recommended-module'],
-}
-
-module.exports = config
