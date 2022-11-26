4eb5473bdddc13923a80e01654c2575b853ac04d
InkoHX <me@inkohx.dev>
2022-11-26 15:49:06 +0900

    chore: apply `@nexterias/eslint-config-node` rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,21 @@ const config = {
     ecmaVersion: 2022,
     sourceType: 'script',
   },
-  extends: [require.resolve('@nexterias/eslint-config-common')],
+  extends: [
+    require.resolve('@nexterias/eslint-config-common'),
+    require.resolve('@nexterias/eslint-config-node'),
+  ],
+  rules: {
+    'n/no-unpublished-require': [
+      'error',
+      {
+        allowModules: [
+          '@nexterias/eslint-config-node',
+          '@nexterias/eslint-config-common',
+        ],
+      },
+    ],
+  },
 }
 
 module.exports = config
